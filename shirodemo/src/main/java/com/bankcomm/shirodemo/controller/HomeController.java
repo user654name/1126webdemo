18774612bc395bb078ed23df9858b21cad0bcf60
user654name <ztqztq@qq.com>
2018-11-26 23:25:36 +0800

    2018年11月26日 23:25:17 登录登出

--- shirodemo/src/main/java/com/bankcomm/shirodemo/controller/HomeController.java
+++ shirodemo/src/main/java/com/bankcomm/shirodemo/controller/HomeController.java
@@ -19,14 +19,25 @@ public class HomeController {
     }
 
 
-    /**
-     * 仅仅为了方便开发，配置直接访问资源
-     *
-     * @param path
-     * @return
-     */
-    @RequestMapping("{path}")
-    public String toAnyWhere(@PathVariable String path) {
-        return path;
+    @RequestMapping("/login")
+    public String toPage1() {
+        return "login";
     }
+
+    @RequestMapping("/notLogin")
+    public String toPage2() {
+        return "notLogin";
+    }
+
+
+//    /**
+//     * 仅仅为了方便开发，配置直接访问资源
+//     *
+//     * @param path
+//     * @return
+//     */
+//    @RequestMapping("{path}")
+//    public String toAnyWhere(@PathVariable String path) {
+//        return path;
+//    }
 }
---
+++ shirodemo/src/main/java/com/bankcomm/shirodemo/controller/ShiroHandler.java
@@ -0,0 +1,50 @@
+package com.bankcomm.shirodemo.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Tianqi.Zhang
+ * @date 2018/11/26
+ * @time 22:00
+ * @package com.bankcomm.shirodemo.controller
+ * @project 1126webdemo
+ * @description
+ */
+
+@Controller
+@RequestMapping("/shiro")
+public class ShiroHandler {
+
+    @RequestMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+
+        System.out.println("username = " + username);
+
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            // 把用户名和密码封装为 UsernamePasswordToken 对象
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            // rememberme
+            token.setRememberMe(false);
+            try {
+                // 执行登录.
+                currentUser.login(token);
+            }
+            // 所有认证时异常的父类.
+            catch (AuthenticationException ae) {
+                //unexpected condition?  error?
+                System.out.println("登录失败: " + ae.getMessage());
+            }
+        }
+        return "login-fail";
+
+    }
+}
+
