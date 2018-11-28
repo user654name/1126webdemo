62962f5ce6681e57605f37d029bc2d2262ee54e9
user654name <ztqztq@qq.com>
2018-11-28 08:14:27 +0800

    2018年11月28日 08:13:45 SecondRealm中配置了临时账户密码实现登录

--- shirodemo/src/main/java/com/bankcomm/shirodemo/shiro/SecondRealm.java
+++ shirodemo/src/main/java/com/bankcomm/shirodemo/shiro/SecondRealm.java
@@ -44,53 +44,14 @@ public class SecondRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         System.out.println("第二个Realm——SecondRealm.doGetAuthenticationInfo");
         System.out.println("————第二个Realm——身份认证方法————");
-        // 1.把AuthenticationInfo转换为PasswordToken
-        UsernamePasswordToken upToken = (UsernamePasswordToken) authenticationToken;
 
-        //2. 从 UsernamePasswordToken 中来获取 username
-        String username = upToken.getUsername();
-
-        //3. 调用数据库的方法, 从数据库中查询 username 对应的用户记录
-        System.out.println("从数据库中获取 username: " + username + " 所对应的用户信息.");
-
-        //4. 若用户不存在, 则可以抛出 UnknownAccountException 异常
-        // "unknown".equals(username)这只是举例子 假设unknown用户
-        if ("unknown".equals(username)) {
-            throw new UnknownAccountException("用户不存在!");
-        }
-
-        //5. 根据用户信息的情况, 决定是否需要抛出其他的 AuthenticationException 异常.
-        // 这也是举例子 这里假设monster用户被锁定
-        if ("monster".equals(username)) {
-            throw new LockedAccountException("用户被锁定");
-        }
-
-        //6. 根据用户的情况, 来构建 AuthenticationInfo 对象并返回. 通常使用的实现类为: SimpleAuthenticationInfo
-        //【说明】以下信息是从数据库中获取的.这里只是模拟
-        //1). principal: 认证的实体信息. 可以是 username, 也可以是数据表对应的用户的实体类对象.
-        Object principal = username;
-        //2). credentials: 密码.
-        Object credentials = null;
-        if ("admin".equals(username)) {
-            credentials = "038bdaf98f2037b31f1e75b5b4c9b26e";
-        } else if ("user".equals(username)) {
-            credentials = "098d2c478e9c11555ce2823231e02ec1";
-        }
-
-        //3). realmName: 当前 realm 对象的 name. 调用父类的 getName() 方法即可
-        String realmName = getName();
-        //4). 盐值
-        String salt = "自定义盐值";
-        /*
-            ByteSource - 接口
-            Util - 内部类
-            bytes() - 返回SimpleByteSource
-            SimpleByteSource - 实现ByteSource接口
-          */
-        ByteSource credentialsSalt = ByteSource.Util.bytes(salt);
 
         SimpleAuthenticationInfo info = null;
-        info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realmName);
+        Object principal = "root";
+        Object credentials = "666888";
+//        ByteSource credentialsSalt= (ByteSource) "";
+        String realmName = "";
+        info = new SimpleAuthenticationInfo(principal, credentials, realmName);
         return info;
         //        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
 //        // 从数据库获取对应用户名密码的用户
